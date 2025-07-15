01375f76e4cffcf62993a9918a8db184dbd41b14
José Neto <sputinykster@gmail.com>
2025-07-15 17:50:05 +0100

    feat(profile): updated metrics

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,54 +1,50 @@
-const React = require('react')
-const { css } = require('glamor')
+const React = require("react");
+const { css } = require("glamor");
 
-const { colors } = require('../variables')
-const SocialMeta = require('./SocialMeta')
+const { colors } = require("../variables");
+const SocialMeta = require("./SocialMeta");
 
-css.global('*', { fontFamily: "'Lato', sans-serif", boxSizing: 'border-box' })
-css.global('html', { scrollBehavior: 'smooth' })
-css.global('body', { margin: 0, paddingTop: 100 })
-css.insert('@media print{body{padding-top: 0}}')
-css.global('img', { userSelect: 'none' })
-css.global('p, a', { fontSize: 16, lineHeight: 1.56, color: colors.secondary })
-css.global('a', { textDecoration: 'none' })
-css.global('a:hover, a[target="_blank"]', { color: colors.primary })
+css.global("*", { fontFamily: "'Lato', sans-serif", boxSizing: "border-box" });
+css.global("html", { scrollBehavior: "smooth" });
+css.global("body", { margin: 0, paddingTop: 100 });
+css.insert("@media print{body{padding-top: 0}}");
+css.global("img", { userSelect: "none" });
+css.global("p, a", { fontSize: 16, lineHeight: 1.56, color: colors.secondary });
+css.global("a", { textDecoration: "none" });
+css.global('a:hover, a[target="_blank"]', { color: colors.primary });
 
-module.exports = ({ children, title, css = '', js = '' }) => (
-    <html lang="en">
-        <head>
-            <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <link
-                rel="shortcut icon"
-                href="/assets/images/favicon.ico"
-                type="image/x-icon"
-            />
-            <link
-                rel="icon"
-                href="/assets/images/favicon.ico"
-                type="image/x-icon"
-            />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css?family=Lato|Roboto"
-            />
-            <style dangerouslySetInnerHTML={{ __html: css }} />
-            <script dangerouslySetInnerHTML={{ __html: js }} />
-            <title>{title}</title>
-            <SocialMeta
-                title={title}
-                url="https://joseneto.dev"
-                description="José Neto's website"
-                image="/assets/images/me.jpg"
-            />
-            <script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=UA-58594179-3"
-            />
-        </head>
-        <body>{children}</body>
-    </html>
-)
+module.exports = ({ children, title, css = "", js = "" }) => (
+  <html lang="en">
+    <head>
+      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <link
+        rel="shortcut icon"
+        href="/assets/images/favicon.ico"
+        type="image/x-icon"
+      />
+      <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Lato|Roboto"
+      />
+      <style dangerouslySetInnerHTML={{ __html: css }} />
+      <script dangerouslySetInnerHTML={{ __html: js }} />
+      <title>{title}</title>
+      <SocialMeta
+        title={title}
+        url="https://joseneto.dev"
+        description="José Neto's website"
+        image="/assets/images/me.jpg"
+      />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-3EY109Y10L"
+      />
+    </head>
+    <body>{children}</body>
+  </html>
+);
--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,20 @@
-window.onload = function() {
-    // Close Drawer after menu item click
-    var btnsNL = document.querySelectorAll('#menu li a')
-    var btns = Array.prototype.slice.call(btnsNL)
-    var dwCtrl = document.querySelector('#drawer-control')
-    btns.forEach(function(btn) {
-        btn.addEventListener('click', function() {
-            dwCtrl.checked = false
-        })
-    })
-}
+window.onload = function () {
+  // Close Drawer after menu item click
+  var btnsNL = document.querySelectorAll("#menu li a");
+  var btns = Array.prototype.slice.call(btnsNL);
+  var dwCtrl = document.querySelector("#drawer-control");
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      dwCtrl.checked = false;
+    });
+  });
+};
 
 // Google Analytics
-window.dataLayer = window.dataLayer || []
+window.dataLayer = window.dataLayer || [];
 function gtag() {
-    dataLayer.push(arguments)
+  dataLayer.push(arguments);
 }
-gtag('js', new Date())
-gtag('config', 'UA-58594179-3')
+gtag("js", new Date());
+
+gtag("config", "G-3EY109Y10L");
